2710b2d26b544c65b481c0307dea212d3ec0302c
Costin Marius <CMarius94@users.noreply.github.com>
2025-05-21 15:55:13 +0300

    Update PriceChecker.cs

--- csharp-test/PriceChecker/PriceChecker.cs
+++ csharp-test/PriceChecker/PriceChecker.cs
@@ -8,7 +8,7 @@ namespace PriceChecker
 		/// <summary>
 		/// Application COLT reads prices in application MSD, between 2 dates and a given product id.
 		/// At one date and for a product id, we have 0 to n prices and these prices can be official or not.
-		/// To have an indicator on price quality and for monitoring purpose, COLT has a control that counts number of dates with more that n official prices by date.
+		/// To have an indicator on price quality and for monitoring purpose, COLT has a control that counts number of dates with more that th official prices by date.
 		/// </summary>
 		/// <returns>
 		/// The number of dates with at least th official prices.
